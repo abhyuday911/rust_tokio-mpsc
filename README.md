2d3684ffef789ea3edc8ecbbd71d6fb3f824eea3
Abhyuday <abhyuday911@gmail.com>
2025-11-12 18:14:06 +0530

    feat: ''/'' route sends the data & the engine receives the data

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Steps
+to have non blocking bla-bla nam bhul gye.
 
-- Create an mpsc channnel 
-- 
+## Version 0
+- Create an mpsc channnel (send and receive at the same place).
+
+## Version 1
+- Send from api route & receive in fn engine.
+
+## Architecture for what needs to be done.
+- You have the mpsc in the main fnc, send the sender to the routes and send the receiver to the engine.
+- another sender & receiver to send things from engine to the route probably.
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder,
+    web::{self, Data},
+};
+use serde_json::json;
 use tokio::sync::mpsc;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("hello from index route")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let (sender, mut receiver) = mpsc::channel(100);
+async fn index(state: Data<AppState>) -> impl Responder {
+    let order_sender = state.order_sender.clone();
 
     tokio::spawn(async move {
         for i in 0..10 {
-            if let Err(_) = sender.send(i).await {
+            if let Err(_) = order_sender.send(i.to_string()).await {
                 println!("receiver dropped");
                 return;
             }
         }
     });
 
+    HttpResponse::Ok().json(json!({"message": "data snent to the engine!"}))
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    order_sender: mpsc::Sender<String>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let (sender, receiver) = mpsc::channel(100);
+
+    tokio::spawn(run_engine(receiver));
+
+    let state = web::Data::new(AppState {
+        order_sender: sender,
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn run_engine(mut receiver: mpsc::Receiver<String>) {
     while let Some(i) = receiver.recv().await {
         println!("got = {}", i)
     }
-
-    HttpServer::new(move || App::new().route("/", web::get().to(index)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
 }
